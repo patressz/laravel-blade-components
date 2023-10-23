6a6689d21c91246066050da0309979ab9b4634d8
Patrik Strišovský <patrik.strisovsky7@gmail.com>
2023-10-23 12:46:10 +0200

    Add `css_framework` key to config to allow change the default style of blade components

--- config/blade-components.php
+++ config/blade-components.php
@@ -2,7 +2,29 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Form Prefix
+    |--------------------------------------------------------------------------
+    |
+    | This value sets the prefix for form components. It allows for clearer
+    | and more organized component naming in your Blade views.
+    |
+    */
+
     'prefix' => [
         'form' => 'form',
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Supported CSS Frameworks
+    |--------------------------------------------------------------------------
+    |
+    | Define the CSS framework you want to use with the components.
+    | Currently, the available options are "tailwind" and "bootstrap".
+    |
+    */
+
+    'css_framework' => 'bootstrap',
 ];
